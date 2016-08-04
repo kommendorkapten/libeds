741be4837934358441d4eeb7b5e95f83ba1b2510
Fredrik Skogman <skogman@gmail.com>
2016-08-04 09:56:06 +0200

    Added hash map stuff.

---
+++ hmap.c
@@ -0,0 +1,290 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "hmap.h"
+
+#define MAX_KEY_LEN 128
+#define STEP_SIZE 1
+#define FLAG_OCCUPIED 0x1
+#define FLAG_DELETED  0x2
+
+/*
+** Count
+** Load factor with re-hash
+** Linear probing
+*/
+
+struct hmap
+{
+	hmap_hash         hfn;
+	hmap_cmp          cfn;
+	struct hmap_node* elems;
+	size_t            cap;
+        size_t            count;
+        float             lfactor;
+};
+
+struct hmap_node
+{
+	void*    key;
+	void*    data;
+#ifdef HMAP_USE_TS
+	time_t   acc_ts;	
+#endif
+	uint32_t hash;
+        int flags;
+};
+
+static uint32_t hmap_default_hash(void* key)
+{
+	/* Jenkin's one at a time hash */
+	char* k = (char*) key;
+	uint32_t hash = 0;
+#ifdef __EXTENSIONS__ 
+        /* strnlen(3C) became availible SUSv4/Posix 2008 */
+	size_t len = strnlen(key, MAX_KEY_LEN);
+#else
+        size_t len = strlen(key);
+        
+        if (len > MAX_KEY_LEN)
+        {
+                len = MAX_KEY_LEN;
+        }
+#endif
+        
+	for (size_t i = 0; i < len; i++)
+	{
+		hash += k[i];
+		hash += (hash <<10);
+		hash ^= (hash >> 6);
+	}
+
+	hash += (hash << 3);
+	hash ^= (hash >> 11);
+	hash += (hash << 15);
+
+	return hash;
+}
+
+static int hmap_default_cmp(void* a, void* b)
+{
+	int r;
+
+	if (a == NULL)
+	{
+		return -1;
+	}
+	if (b == NULL)
+	{
+		return 1;
+	}
+	
+	r = strncmp((char*)a, (char*)b, MAX_KEY_LEN);
+
+	return r;
+}
+
+struct hmap* hmap_create(hmap_hash hfn, hmap_cmp cfn, size_t cap, float lf)
+{
+	struct hmap* h = malloc(sizeof(struct hmap));
+
+	if (hfn == NULL)
+	{
+		hfn = &hmap_default_hash;
+	}
+	if (cfn == NULL)
+	{
+		cfn = &hmap_default_cmp;
+	}
+
+	h->hfn = hfn;
+	h->cfn = cfn;
+	h->cap = cap;
+	h->elems = malloc(cap * sizeof(struct hmap_node));
+	h->count = 0;
+	h->lfactor = lf;
+	memset(h->elems, 0, cap * sizeof(struct hmap_node));
+	return h;
+}
+
+void hmap_destroy(struct hmap* h)
+{
+	free(h->elems);
+	free(h);
+}
+
+void hmap_set(struct hmap* h, void* key, void* data)
+{
+	uint32_t k = h->hfn(key);
+	size_t spos = k % h->cap;
+	size_t pos = spos;
+        int added = 1;
+
+	while (h->elems[pos].flags & FLAG_OCCUPIED)
+	{
+		if (h->elems[pos].hash == k && 
+                    h->cfn(h->elems[pos].key, key) == 0)
+		{
+                        /* Replace value */
+                        added = 0;
+			break;
+		}
+
+		/* Use linear probing */
+		pos = (pos + 1) % h->cap;
+
+		if (pos == spos)
+		{
+			/* We are back at the beginning, 
+			   should NEVER happen */
+			abort();
+		}
+	}
+
+	h->elems[pos].key = key;	
+	h->elems[pos].data = data;
+	h->elems[pos].hash = k;
+	h->elems[pos].flags = FLAG_OCCUPIED;
+
+        if (added)
+        {
+                float lfactor;
+                h->count++;
+                lfactor = ((float)h->count) / h->cap;
+
+                if (lfactor > h->lfactor)
+                {
+                        size_t ocap = h->cap;
+
+                        /* Extend capacity by two */
+                        h->cap *= 2;
+                        h->count = 0;
+
+                        /* allocate more data and re-hash */
+                        struct hmap_node* new;
+                        struct hmap_node* old;
+                        size_t new_s = h->cap * sizeof(struct hmap_node);
+                        
+                        new = malloc(new_s);
+                        memset(new, 0, new_s);
+
+                        old = h->elems;
+                        h->elems = new;
+
+                        for (size_t i = 0; i < ocap; i++)
+                        {
+                                if (old[i].flags & FLAG_OCCUPIED)
+                                {
+                                        hmap_set(h, old[i].key, old[i].data);
+                                }
+                        }
+
+                        free(old);
+                }
+        }
+}
+
+void* hmap_get(const struct hmap* h, void* key)
+{
+	uint32_t k = h->hfn(key);
+	size_t spos = k % h->cap;
+	size_t pos = spos;
+
+	if (h->cfn(h->elems[pos].key, key) == 0)
+	{
+                if (h->elems[pos].flags & FLAG_OCCUPIED)
+                {
+                        return h->elems[pos].data;
+                }
+	}
+
+	/* Do a linear probe, need to scan over deleted entries too */
+	pos = (pos + 1) % h->cap;
+	while (h->elems[pos].flags)
+	{
+                if (h->elems[pos].flags & FLAG_OCCUPIED)
+                {
+                        if (h->elems[pos].hash == k && 
+                            h->cfn(h->elems[pos].key, key) == 0)
+                        {
+                                return h->elems[pos].data;
+                        }
+                }
+
+		pos = (pos + 1) % h->cap;
+		
+		if (pos == spos)
+		{
+			/* We are back at the beginning */
+			break;
+		}
+	}
+	
+	return NULL;
+}
+
+void hmap_del(struct hmap* h, void* key)
+{
+	uint32_t k = h->hfn(key);
+	size_t spos = k % h->cap;
+	size_t pos = spos;
+
+	if (h->cfn(h->elems[pos].key, key) == 0)
+	{
+                h->count--;
+		h->elems[pos].flags = FLAG_DELETED;
+                return;
+	}
+
+	/* Do a linear probe */
+	pos = (pos + 1) % h->cap;
+	while (h->elems[pos].flags & FLAG_OCCUPIED)
+	{
+		if (h->elems[pos].hash == k && 
+                    h->cfn(h->elems[pos].key, key) == 0)
+		{
+                        h->count--;
+                        h->elems[pos].flags = FLAG_DELETED;
+			return;
+		}
+		pos = (pos + 1) % h->cap;
+		
+		if (pos == spos)
+		{
+			/* We are back at the beginning */
+			break;
+		}
+	}
+	
+	return;
+}
+
+size_t hmap_size(const struct hmap* h)
+{
+        return h->count;
+}
+
+size_t hmap_cap(const struct hmap* h)
+{
+        return h->cap;
+}
+
+struct hmap_entry* hmap_iter(const struct hmap* h, size_t* count)
+{
+        struct hmap_entry* e = malloc(h->count * sizeof(struct hmap_entry));
+        size_t p = 0;
+
+        for (size_t i = 0; i < h->cap; i++)
+        {
+                if (h->elems[i].flags & FLAG_OCCUPIED)
+                {
+                        e[p].key = h->elems[i].key;
+                        e[p].data = h->elems[i].data;
+                        p++;
+                }
+        }
+        
+        *count = h->count;
+
+        return e;
+}
---
+++ hmap_test.c
@@ -0,0 +1,279 @@
+#include "hmap.h"
+#include <scut.h>
+#include <stdlib.h>
+#include <string.h>
+
+int test_hmap_create(void);
+int test_hmap_get_set(void);
+int test_hmap_del(void);
+int test_hmap_expand(void);
+int test_hmap_iter(void);
+
+uint32_t const_hash(void* key)
+{
+        return 1;
+}
+
+int main(void)
+{
+        int ret;
+        
+        scut_create("Test Hash table");
+        
+        SCUT_ADD(test_hmap_create);
+        SCUT_ADD(test_hmap_get_set);
+        SCUT_ADD(test_hmap_del);
+        SCUT_ADD(test_hmap_expand);
+        SCUT_ADD(test_hmap_iter);
+
+        ret = scut_run(0);
+
+        return ret;
+}
+
+int test_hmap_create(void)
+{
+        struct hmap* h = hmap_create(NULL, NULL, 128, 0.7);
+        struct hmap_entry* i;
+        size_t count;
+        
+        SCUT_ASSERT_IE(hmap_size(h), 0);
+        i = hmap_iter(h, &count);
+
+        if (count)
+        {
+                SCUT_FAIL("Non empty list returned");
+        }
+
+        free(i);
+        hmap_destroy(h);
+
+        return 0;
+}
+
+int test_hmap_get_set(void)
+{
+        struct hmap* h = hmap_create(NULL, NULL, 128, 0.7);
+        
+        SCUT_ASSERT_IE(hmap_size(h), 0);
+
+        hmap_set(h, "a", (void*)1l);
+        hmap_set(h, "aa", (void*)2l);
+        hmap_set(h, "aaa", (void*)3l);
+
+        SCUT_ASSERT_IE(hmap_size(h), 3);
+        SCUT_ASSERT_IE(hmap_cap(h), 128);
+
+        SCUT_ASSERT_IE(hmap_get(h, "a"), (void*)1l);
+        SCUT_ASSERT_IE(hmap_get(h, "aa"), (void*)2l);
+        SCUT_ASSERT_IE(hmap_get(h, "aaa"), (void*)3l);
+
+        SCUT_ASSERT_IE(hmap_get(h, "aaaa"), NULL);
+
+        /* Replace a value */
+        hmap_set(h, "aa", (void*)123l);
+        SCUT_ASSERT_IE(hmap_size(h), 3);
+        SCUT_ASSERT_IE(hmap_get(h, "aa"), (void*)123l);
+
+        hmap_destroy(h);
+
+        return 0;
+}
+
+int test_hmap_del(void)
+{
+        struct hmap* h = hmap_create(NULL, NULL, 128, 0.7);
+        
+        SCUT_ASSERT_IE(hmap_size(h), 0);
+
+        hmap_set(h, "a", (void*)1l);        
+        SCUT_ASSERT_IE(hmap_get(h, "a"), (void*)1l);
+        SCUT_ASSERT_IE(hmap_size(h), 1);
+
+        hmap_del(h, "a");
+        SCUT_ASSERT_IE(hmap_get(h, "a"), NULL);
+        SCUT_ASSERT_IE(hmap_size(h), 0);
+
+        hmap_destroy(h);
+
+        /* Test with deleted entries */
+        h = hmap_create(&const_hash, NULL, 128, 0.7);
+
+        hmap_set(h, "a", (void*)100L);
+        hmap_set(h, "b", (void*)200L);
+        hmap_set(h, "c", (void*)300L);
+        hmap_set(h, "d", (void*)400L);
+        SCUT_ASSERT_IE(hmap_size(h), 4);
+
+        SCUT_ASSERT_IE(hmap_get(h, "a"), (void*)100L);
+        SCUT_ASSERT_IE(hmap_get(h, "b"), (void*)200L);
+        SCUT_ASSERT_IE(hmap_get(h, "c"), (void*)300L);
+        SCUT_ASSERT_IE(hmap_get(h, "d"), (void*)400L);
+
+        hmap_del(h, "a");
+        SCUT_ASSERT_IE(hmap_size(h), 3);
+
+        SCUT_ASSERT_IE(hmap_get(h, "a"), NULL);
+        SCUT_ASSERT_IE(hmap_get(h, "b"), (void*)200L);
+        SCUT_ASSERT_IE(hmap_get(h, "c"), (void*)300L);
+        SCUT_ASSERT_IE(hmap_get(h, "d"), (void*)400L);
+
+        hmap_del(h, "c");
+        SCUT_ASSERT_IE(hmap_size(h), 2);
+
+        SCUT_ASSERT_IE(hmap_get(h, "a"), NULL);
+        SCUT_ASSERT_IE(hmap_get(h, "b"), (void*)200L);
+        SCUT_ASSERT_IE(hmap_get(h, "c"), NULL);
+        SCUT_ASSERT_IE(hmap_get(h, "d"), (void*)400L);
+
+        hmap_set(h, "a", (void*)666L);
+        hmap_set(h, "c", (void*)777L);
+
+        /* Oh lord. Things are getting ugly. */
+        char* elems = (char*)h;
+        long val;
+        /* Step up to elems ptr */
+        elems = elems + 16;
+        /* follow up elems ptr */
+        elems = *(char**)elems;
+
+        /* data is at offset 8 */
+        elems += 8;
+
+        /* first and sixth slot should be empty */
+        /* Expected data is 0, 666, 200, 777, 400, 0 */
+
+        /* node size is 24 bit on 64b system */
+        memcpy(&val, elems, sizeof(long));
+        SCUT_ASSERT_IE(val, 0L);
+        memcpy(&val, elems + 24, sizeof(long));
+        SCUT_ASSERT_IE(val, 666L);
+        memcpy(&val, elems + 48, sizeof(long));
+        SCUT_ASSERT_IE(val, 200L);
+        memcpy(&val, elems + 72, sizeof(long));
+        SCUT_ASSERT_IE(val, 777L);
+        memcpy(&val, elems + 96, sizeof(long));
+        SCUT_ASSERT_IE(val, 400L);        
+        memcpy(&val, elems + 120, sizeof(long));
+        SCUT_ASSERT_IE(val, 0L);
+        
+        hmap_destroy(h);
+
+        return 0;
+}
+
+int test_hmap_expand(void)
+{
+        struct hmap* h = hmap_create(NULL, NULL, 16, 0.7);        
+        long values[14] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+        char* keys[14] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+                          "10", "11", "12", "13"};
+        size_t elems = sizeof(keys) / sizeof(char*);
+        size_t count = 0;
+        struct hmap_entry* entries;
+        
+        /* 11 first should be ok */
+
+        for (size_t i = 0; i < 11; i++)
+        {
+                hmap_set(h, keys[i], (void*)values[i]);
+        }
+
+        SCUT_ASSERT_IE(hmap_size(h), 11);
+        SCUT_ASSERT_IE(hmap_cap(h), 16);
+
+        /* add rest, expect expansion */
+        for (size_t i = 11; i < elems; i++)
+        {
+                hmap_set(h, keys[i], (void*)values[i]);                
+        }
+
+        SCUT_ASSERT_IE(hmap_size(h), 14);
+        SCUT_ASSERT_IE(hmap_cap(h), 32);
+
+        /* Make sure all elems are present */
+        for (size_t i = 0; i < elems; i++)
+        {
+                void* v = hmap_get(h, keys[i]);
+                SCUT_ASSERT_IE(v, values[i]);
+        }
+
+        /* Iterate over all elems as well */
+        entries = hmap_iter(h, &count);
+        SCUT_ASSERT_IE(count, elems);
+
+        for (size_t i = 0; i < elems; i++)
+        {
+                int found = 0;
+
+                for (size_t j = 0; j < count; j++)
+                {
+                        if (values[i] == (long)entries[j].data &&
+                            strncmp(keys[i], entries[j].key, 2) == 0)
+                        {
+                                found = 1;
+                                break;
+                        }
+                }
+
+                if (!found)
+                {
+                        char s[32];
+                        snprintf(s, 32, "Could not find %ld", values[i]);
+                        SCUT_FAIL(s);
+                }
+        }
+        
+
+        hmap_destroy(h);
+        free(entries);
+
+        return 0;
+}
+
+int test_hmap_iter(void)
+{
+        struct hmap* h = hmap_create(NULL, NULL, 128, 0.7);
+        struct hmap_entry* data;
+        size_t count;
+        long exp[4] = {100L, 200L, 300L, 400L};
+        char* keys[4] = {"a", "b", "c", "d" };
+        size_t elems = sizeof(exp)/sizeof(long);
+
+        hmap_set(h, "a", (void*)100L);
+        hmap_set(h, "b", (void*)200L);
+        hmap_set(h, "c", (void*)300L);
+        hmap_set(h, "d", (void*)400L);
+        SCUT_ASSERT_IE(hmap_size(h), 4);
+
+        data = hmap_iter(h, &count);
+
+        SCUT_ASSERT_IE(count, elems);
+
+        for (size_t i = 0; i < elems; i++)
+        {
+                int found = 0;
+                
+                for (size_t j = 0; j < count; j++)
+                {
+                        if (exp[i] == (long)data[j].data &&
+                            strcmp(keys[i], data[j].key) == 0)
+                        {
+                                found = 1;
+                                break;
+                        }
+                }
+
+                if (!found)
+                {
+                        char s[32];
+                        snprintf(s, 32, "Could not find %ld", exp[i]);
+                        SCUT_FAIL(s);
+                }
+        }
+
+        hmap_destroy(h);
+        free(data);
+
+        return 0;
+}
